a2059deb4440452dc3011174749feb5804b8850d
duvivierbri <duvivierbri@gmail.com>
2022-04-07 22:34:10 -0400

    Created Gameboard.css

--- cutewordleapp/src/components/Gameboard.js
+++ cutewordleapp/src/components/Gameboard.js
@@ -1,10 +1,57 @@
 import React from "react";
+import './Gameboard.css';
 
 class Gameboard extends React.Component {
-
     render () {
         return (
-            <h1>Gameboard</h1>
+            <div className = "gameGrid">
+                <thead>
+                    <tr>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                    </tr>
+                    <tr>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                    </tr>
+                    <tr>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                    </tr>
+                    <tr>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                    </tr>
+                    <tr>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                    </tr>
+                    <tr>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                        <th>X</th>
+                    </tr>
+                </thead>
+            </div>
+            
+            
         );
     }
 }
