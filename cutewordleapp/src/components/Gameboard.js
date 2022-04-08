e5d21f9635640e3d7f605bf207af90296adc4fc5
duvivierbri <duvivierbri@gmail.com>
2022-04-07 20:54:42 -0400

    Created GameBoard.js, Keyboard.js, and Game.js

---
+++ cutewordleapp/src/components/Gameboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Gameboard extends React.Component {
+
+    render () {
+        return (
+            <h1>Gameboard</h1>
+        );
+    }
+}
+
+export default Gameboard;
---
+++ cutewordleapp/src/components/Keyboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Keyboard extends React.Component {
+
+    render () {
+        return (
+            <h1>Keyboard</h1>
+        );
+    }
+}
+
+export default Keyboard;
