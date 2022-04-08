082f8067e9c7d6568d3975278c4a620e14c42867
duvivierbri <duvivierbri@gmail.com>
2022-04-07 22:12:55 -0400

    Created the keyboard

--- cutewordleapp/src/components/Keyboard.js
+++ cutewordleapp/src/components/Keyboard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { ReactDOM } from "react";
 
 class Keyboard extends React.Component {
-
     render () {
-        return (
-            <h1>Keyboard</h1>
+        const row1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+        const row2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+        const row3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+        return(
+            <div>
+                {row1.map(letter => <button>{letter}</button>)}
+                <br/>
+                {row2.map(letter => <button>{letter}</button>)}
+                <br/>
+                {row3.map(letter => <button>{letter}</button>)}
+            </div>
         );
     }
 }
