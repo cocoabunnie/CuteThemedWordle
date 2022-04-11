291e743695460d8c3ae71185f2ed3eae0cb945ad
duvivierbri <duvivierbri@gmail.com>
2022-04-11 13:29:14 -0400

    Made default board value as a 2d array

--- cutewordleapp/src/components/Gameboard.js
+++ cutewordleapp/src/components/Gameboard.js
@@ -3,52 +3,18 @@ import './Gameboard.css';
 
 class Gameboard extends React.Component {
     render () {
+        const boardValue = [
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""],
+            ["", "", "", "", ""]
+        ]
+
         return (
             <div className = "gameGrid">
-                <thead>
-                    <tr>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                    </tr>
-                    <tr>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                    </tr>
-                    <tr>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                    </tr>
-                    <tr>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                    </tr>
-                    <tr>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                    </tr>
-                    <tr>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                        <th>X</th>
-                    </tr>
-                </thead>
+                
             </div>
             
             
